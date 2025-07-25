b6f6fd1545e073f8b247e7984c90977153ca4a34
Mayank Saharkar <mayanksaharkar@gmail.com>
2025-07-25 11:15:42 +0530

    setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function App() {
+  return (
+    <>
+      <div className="text-4xl flex justify-center items-center h-screen font-bold text-red-700">
+        Automated Question Paper Generator App
+      </div>
+    </>
+  );
+}
+
+export default App;
